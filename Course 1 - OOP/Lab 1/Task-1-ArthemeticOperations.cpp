d06cbf6d4eebd83e8263c8c5ea500e44053cfe64
Mawadah Elmashad <104011518+mawadaelmeshad@users.noreply.github.com>
2025-10-19 09:11:24 +0300

    Create  Task-1-ArthemeticOperations.cpp

---
+++ Course 1 - OOP/Lab 1/Task-1-ArthemeticOperations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cin>>num1;
+    cin>>num2;
+    cout<<"sum: "<<num1+num2<<endl;
+    cout<<"sub: "<<num1-num2<<endl;
+    cout<<"mul: "<<num1*num2<<endl;
+    cout<<"div: "<<num1/num2<<endl;
+
+    return 0;
+}
