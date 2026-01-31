3e36a0b1e01f5f2329eb6d8af39f82fc3b3ea4cc
mawadaelmeshad <mawadahassan21@gmail.com>
2026-01-31 12:26:50 +0200

    java lab 1 uploaded

---
+++ Course-Java/Lab1/Task2/src/task2/Task2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package task2;
+
+/**
+ *
+ * @author mawad
+ */
+public class Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int i=0;i<args.length;i++){
+           System.out.println(args[i]);
+       }
+    }
+    
+}
---
+++ Course-Java/Lab1/Task3/src/task3/Task3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package task3;
+
+/**
+ *
+ * @author mawad
+ */
+public class Task3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for(int i=0;i<n;i++){
+            System.out.println(args[1]);
+        }
+    }
+    
+}
