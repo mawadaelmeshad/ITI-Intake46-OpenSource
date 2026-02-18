6caab03d42b0ad6fbc8c46c34a4b84913d7bf75f
mawadaelmeshad <mawadahassan21@gmail.com>
2026-02-18 14:32:51 +0200

    lab 1 xml,json added

---
+++ Course-xml&json/Lab1/task1-xml/src/main/java/com/mycompany/task1/xml/Task1Xml.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.task1.xml;
+
+/**
+ *
+ * @author mawad
+ */
+public class Task1Xml {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
